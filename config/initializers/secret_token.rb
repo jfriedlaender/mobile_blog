da981737e91ab258f08db903cbe665fc427d8687
Joel Friedlaender <joel@redguava.com.au>
2011-05-23 12:03:05 +1000

    App created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileBlog::Application.config.secret_token = '646a207e55619a5620117edca3fd32070e2134fcba89b6f10d31660d1e68ad660426adcb68c3997de501d182a88bf08f491e9b1049ff73dd0c7ad2e0ac71d026'
